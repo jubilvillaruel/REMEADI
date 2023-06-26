39e3247e99f7e9eeac1a0d96315b607ec090228a
bossjubs <jubvillaruel@gmail.com>
2023-06-26 21:22:44 +0800

    initialized navigation of screen in App.js

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Landing from './src/screens/LandingPage';
+// import SignIn from './src/screens/SignIn';
+import SignIn from './src/screens/SignIn';
+import SignUp from './src/screens/SignUp';
+import Verification from './src/screens/Verification';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing" screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.5,
+            shadowRadius: 3,
+          },
+          headerTintColor: '#2EC4B6',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 20,
+          }
+      }}>
+        <Stack.Screen name="Landing" component={Landing} options={{headerShown: false}}/>
+        <Stack.Screen name="SignIn" component={SignIn} options={{title: 'Sign In'}}/>
+        <Stack.Screen name="Sign_Up" component={SignUp} options={{title: 'Sign Up'}}/>
+        <Stack.Screen name="Verification" component={Verification} options={{title: 'Verification'}}/>
+        <Stack.Screen name="Home" component={Home} options={{title: 'Home'}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/LandingPage.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <View>
+      <Text>LandingPage</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
