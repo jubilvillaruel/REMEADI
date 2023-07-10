6e5138cdc2edd9dd7806d595ed4d6397bed39e1e
rinuld <rjcanlas21@gmail.com>
2023-07-11 02:26:30 +0800

    updated session, now with bgm options and summary

--- src/screens/Session.js
+++ src/screens/Session.js
@@ -1,9 +1,10 @@
 import React, { useState }from 'react'
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ScrollView, Image, Modal } from 'react-native';
 import { StepCard } from '../components/cards';
 import { screenWidth, screenHeight } from '../components/dimensions';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+import { PrimaryButton } from '../components/buttons';
 import FlipCard from 'react-native-flip-card';
 import music from '../../assets/images/music.png';
 import text from '../../assets/images/text.png';
@@ -11,15 +12,41 @@ import video from '../../assets/images/video.png';
 
 import { styles } from '../../assets/css/Style';
 
-export default function Session({  }) {
+export default function Session({ navigation }) {
     const [isFlipped, setIsFlipped] = useState(false);
     // Set either stopwatch or timer to true based on practice.
     const [isStopwatchVisible] = useState(true);
     const [isTimerVisible] = useState(false);
-
+    const [msgVisible, setMsgVisible] = useState(false);
+    const [bgmVisible, setBgmVisible] = useState(false);
+    const [selectedItems, setSelectedItems] = useState([]);
 
     const flip = () => {
         setIsFlipped(!isFlipped);
+    };
+
+    const showMsgModal = () => {
+        setMsgVisible(true);
+    };
+    
+    const backToHome = () => {
+        navigation.navigate('Home');
+    };
+
+    const showBgmModal = () => {
+        setBgmVisible(true);
+    };
+    
+    const hideBgmModal = () => {
+        setBgmVisible(false);
+    };
+
+    const toggleItem = (item) => {
+        if (selectedItems.includes(item)) {
+            setSelectedItems(selectedItems.filter((selectedItem) => selectedItem !== item));
+        } else {
+            setSelectedItems([...selectedItems, item]);
+        }
     };
 
     return (
@@ -27,7 +54,7 @@ export default function Session({  }) {
             <Text style={[styles.bold, styles.colorPrimary, { fontSize: RFPercentage(3), margin: 15 }]}>Title</Text>
 
             <View style={inStyles.headerContainer}>
-                <View style={[styles.dropShadow, styles.bgColorPrimary, inStyles.timerContainer]}>
+                <View style={[styles.bgColorPrimary, inStyles.timerContainer]}>
                     {isStopwatchVisible && (
                         <Stopwatch
                             start={true}
@@ -49,8 +76,8 @@ export default function Session({  }) {
                         />
                     )}
                 </View>
-                <View style={inStyles.mediaContainer}>
-                    <TouchableOpacity style={[styles.dropShadow, inStyles.btnMedia]}>
+                <View style={{ flexDirection: 'row', gap: 15, }}>
+                    <TouchableOpacity style={[styles.dropShadow, inStyles.btnMedia]} onPress={showBgmModal}>
                         <Image style={[{ width: 40, height: 40 }]} source={music}/>
                     </TouchableOpacity>
 
@@ -64,7 +91,7 @@ export default function Session({  }) {
                 </View>
             </View>
             
-            <View style={[styles.dropShadow, inStyles.guideContainer]}>
+            <View style={inStyles.guideContainer}>
                 <FlipCard
                     friction={6}
                     perspective={1000}
@@ -72,7 +99,7 @@ export default function Session({  }) {
                     flipVertical={false}
                     flip={isFlipped}
                     clickable={false}>
-                    <ScrollView showsVerticalScrollIndicator={false} style={inStyles.stepsContainer}>
+                    <ScrollView showsVerticalScrollIndicator={false} style={{ paddingHorizontal: 5 }}>
                         <StepCard title='Step 1' desc='Content'></StepCard>
                         <StepCard title='Step 1' desc='Content'></StepCard>
                         <StepCard title='Step 1' desc='Content'></StepCard>
@@ -91,15 +118,75 @@ export default function Session({  }) {
             </View>
 
             <View style={inStyles.bottomContainer}>
-                <TouchableOpacity style={[styles.dropShadow, inStyles.btnEnd]}>
-                    <Text style={[{ fontSize: RFPercentage(3) }, styles.colorPrimary, styles.bold]}>Finish</Text>
+                <TouchableOpacity style={[styles.dropShadow, inStyles.btnEnd]} onPress={showMsgModal}>
+                    <Text style={[{ fontSize: RFPercentage(3) }, styles.colorPrimary, styles.bold]}>Done</Text>
                 </TouchableOpacity>
             </View>
 
+            <Modal visible={msgVisible} animationType='fade' transparent={true}>
+                <View style={inStyles.summaryContainer}>
+                    <View style={[inStyles.summaryContent, styles.dropShadow, { gap: 15 }]}>
+                        <Text style={[styles.bold, { fontSize: RFPercentage(3) }]}>Session Done!</Text>
+
+                        <View style={{ gap: 5 }}>
+                            <Text style={{ fontSize: RFPercentage(2) }}>Title</Text>
+                            <View style={inStyles.infoContainer}>
+                                <Text style={{ fontSize: RFPercentage(2) }}>Meditation Type:</Text>
+                                <Text style={{ fontSize: RFPercentage(2) }}>Spiritual</Text>
+                            </View>
+                            <View style={inStyles.infoContainer}>
+                                <Text style={{ fontSize: RFPercentage(2) }}>Duration:</Text>
+                                <Text style={{ fontSize: RFPercentage(2) }}>10 minutes</Text>
+                            </View>
+                            <View style={inStyles.infoContainer}>
+                                <Text style={{ fontSize: RFPercentage(2) }}>Times Practiced:</Text>
+                                <Text style={{ fontSize: RFPercentage(2) }}>5</Text>
+                            </View>
+                        </View>
+
+                        <PrimaryButton
+                            text='Back to Home'
+                            textColor= '#FFFFFF'
+                            textSize={RFPercentage(2.2)}
+                            width={screenWidth('70%')}
+                            height={screenHeight('7%')}
+                            borderRad={30}
+                            onPress={backToHome}>
+                        </PrimaryButton>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal visible={bgmVisible} animationType='slide' transparent={true}>
+                <View style={inStyles.bgmContainer}>
+                    <View style={[inStyles.bgmContent, { gap: 15 }]}>
+                        <Text style={[styles.bold, { fontSize: RFPercentage(2) }]}>Background Music</Text>
+
+                        <ScrollView style={inStyles.bgmListContainer}>
+                            <TouchableOpacity onPress={() => toggleItem('Item 1')}>
+                                <Text style={[inStyles.itemText, selectedItems.includes('Item 1') && inStyles.selectedItemText]}>
+                                    Item 1
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => toggleItem('Item 2')}>
+                                <Text style={[inStyles.itemText, selectedItems.includes('Item 2') && inStyles.selectedItemText]}>
+                                    Item 2
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => toggleItem('Item 3')}>
+                                <Text style={[inStyles.itemText, selectedItems.includes('Item 3') && inStyles.selectedItemText]}>
+                                    Item 3
+                                </Text>
+                            </TouchableOpacity>
+                        </ScrollView>
+
+                        <TouchableOpacity onPress={hideBgmModal}>
+                            <Text style={[styles.bold, { fontSize: RFPercentage(2) }]}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </SafeAreaView>
-
-        
-
     )
 }
 
@@ -113,11 +200,6 @@ const inStyles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    mediaContainer: {
-        flexDirection: 'row',
-        gap: 15,
-    },
-
     guideContainer: {
         width: screenWidth('90%'),
         height: screenHeight('70%'),
@@ -125,10 +207,6 @@ const inStyles = StyleSheet.create({
         borderWidth: 2,
         borderColor: '#2EC4B6',
         borderRadius: 10,
-    },
-
-    stepsContainer: {
-        paddingHorizontal: 5,
     },
 
     bottomContainer: {
@@ -176,5 +254,76 @@ const inStyles = StyleSheet.create({
         color: '#CBF3F0',
         fontSize: RFPercentage(2),
         fontWeight: 'bold',
+    },
+
+    summaryContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    },
+
+    summaryContent: {
+        width: screenWidth('90%'),
+        height: screenHeight('40%'),
+        padding: 15,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    
+    bgmContainer: {
+        textAlign: 'center',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1,
+    },
+
+    bgmContent: {
+        bottom: 0,
+        width: screenWidth('100%'),
+        height: screenHeight('40%'),
+        padding: 15,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: -3,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 3,
+    },
+
+    bgmListContainer: {
+        width: screenWidth('90%'),
+        height: screenHeight('15%'),
+        padding: 15,
+        gap: 15,
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#2EC4B6',
+    },
+
+    itemText: {
+        fontSize: RFPercentage(2),
+        paddingVertical: 5,
+        flex: 1,
+    },
+    
+    selectedItemText: {
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        backgroundColor: '#FFBF69',
+    },
+    
+    infoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        gap: 75,
     },
 });
