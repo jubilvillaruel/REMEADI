bd871c580486b3a3fa95fca73dc73b98b4af8f4e
rinuld <rjcanlas21@gmail.com>
2023-07-22 00:17:47 +0800

    added fetching video from database

--- src/screens/Extensions/Video.js
+++ src/screens/Extensions/Video.js
@@ -1,19 +1,49 @@
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { Video } from 'expo-av';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { screenHeight, screenWidth } from '../../components/Dimensions';
-import { RFPercentage } from 'react-native-responsive-fontsize';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const VideoPlayer = () => {
     const video = React.useRef(null);
     const [status, setStatus] = React.useState({});
   
+    const fetchVideoUrl = async () => {
+        try {
+            const storage = getStorage();
+            const videoRef = ref(storage, 'rosary/mysteries/Sorrowful Mysteries of the Holy Rosary.mp4');
+            const videoDownloadURL = await getDownloadURL(videoRef);
+            return videoDownloadURL;
+        } catch (error) {
+            console.error("Error fetching video URL:", error);
+            return null;
+        }
+    };
+
+    const [videoUrl, setVideoUrl] = useState(null);
+
+    useEffect(() => {
+        const getVideoUrl = async () => {
+            const url = await fetchVideoUrl();
+            setVideoUrl(url);
+        };
+        getVideoUrl();
+    }, []);
+
+    if (!videoUrl) {
+        return (
+            <View style={inStyles.placeholder}>
+                <Text>Fetching Rosary of the Day...</Text>
+            </View>
+        );
+    }
+
     return (
         <Video
             ref={video}
             videoStyle={inStyles.video}
             style={inStyles.videoContainer}
-            source={require('../../../assets/videos/hehehhehee.mp4')}
+            source={{ uri: videoUrl }}
             useNativeControls={true}
             resizeMode="contain"
             onPlaybackStatusUpdate={setStatus}
@@ -27,6 +57,13 @@ const inStyles = StyleSheet.create({
         height: screenHeight('50%'),
         justifyContent: 'center',
         backgroundColor: 'rgba(35, 35, 35, 0.5)',
+    },
+
+    placeholder: {
+        width: screenWidth('100%'),
+        height: screenHeight('50%'),
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
     video: {
